733507563f9e84a580615ef76f89e140a13d39f3
tamagsantos <tamarags.ve@hotmail.com>
2025-03-23 22:14:01 -0300

    Correções do código

--- agendacripto.java
+++ agendacripto.java
@@ -4,7 +4,17 @@ public class agendacripto {
 public static void main(String[]ags){
   
     //Solicitando entrada do usuário
-    String entrada = JOptionPane.showInputDialog(null,"Digite uma seguencia de caracteres para criptografar:");
+    String entrada = JOptionPane.showInputDialog(null, "Olá, insira uma String de entrada: ");
+
+    if (entrada == null || entrada.isEmpty()) {
+        JOptionPane.showMessageDialog(null, "Nenhuma informação foi inserida, revise por favor!");
+        return; // Saindo para correção caso não for inserido algo.
+    }
+
+    JOptionPane.showMessageDialog(null, entrada);
+
+    // Determinando o tamanho da string 
+    int tamanho = entrada.length();
     
     //Convertendo a string em um vetor de caracteres
     char[] caracteres = entrada.toCharArray();
@@ -16,6 +26,6 @@ public static void main(String[]ags){
         criptografado.append((char) ascii); //Converte de volta para carectere e adiciona a string cripto
     }
 
-    JOptionPane.showMessageDialog(null, "String critografada:" + criptografado.toString());
+    JOptionPane.showMessageDialog(null, "String critografada:\n" + criptografado.toString() + "\nTamanho da String original:" + tamanho);
 }
 }
